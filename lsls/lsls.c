924a534abd761b3aaa475148c587fd4b8a20fdc1
Ta1grr <taile602@gmail.com>
2019-03-31 21:48:59 -0500

    Finally got the single . to output properly, working on bigger outputs

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -25,28 +25,40 @@ int main(int argc, char **argv)
     // Assigning directory with opendir depending on argc equal to 1 or not.
     // DIR *directory = argc == 1 ? opendir(".") : opendir(*argv);
 
-    directory = argc == 1 ? opendir(".") : opendir(*argv);
+    // directory = argc == 1 ? opendir(".") : opendir(*argv);
     // Assigning ent address to readdir()
-    ent = readdir(directory);
+    // ent = readdir(directory);
 
 
 
     // If the directory is NULL then return the error message
-    if (directory == NULL){
-      fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
-      return -1;
-    }
     // if (argc == 1) {
       
 
     //   return 0;
     // }
     // else print the names
+    if (argc == 1) {
+      directory = opendir(".");
+      if (directory == NULL){
+        fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
+      }
+      while ((ent = readdir(directory))){
+        printf("%s\n", ent->d_name);
+      }
+      closedir(directory);
+      return 0;
+    }
+
     for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
-        // directory = opendir(argv[i]);
+        directory = opendir(argv[i]);
         // ent = readdir(directory);
-        while(ent){
+        if (directory == NULL){
+          fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
+          return -1;
+        }
+        while((ent = readdir(directory))){
           puts(ent->d_name);
         }
         closedir(directory);
