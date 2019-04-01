b0b93127a93979bd657a02f9541f79f5529bb730
Ta1grr <taile602@gmail.com>
2019-04-01 00:23:49 -0500

    both looks similar and gonna make it into a function

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -8,40 +8,19 @@
 int main(int argc, char **argv)
 {
   // Parse command line
-
-    // Calling the struct dirent to the pointer *ent
     struct dirent *ent;
 
-  // if (argc == 1){
+    // DIR *directory;
 
-  // }
-
-  // for (int i = 0; i < argc; i++) {
-  //   printf("%s\n", argv[i]);
-
-  // }
-    DIR *directory;
-
-    // Assigning directory with opendir depending on argc equal to 1 or not.
-    // DIR *directory = argc == 1 ? opendir(".") : opendir(*argv);
-
-    // directory = argc == 1 ? opendir(".") : opendir(*argv);
-    // Assigning ent address to readdir()
-    // ent = readdir(directory);
-
-
-
-    // If the directory is NULL then return the error message
-    // if (argc == 1) {
-      
-
-    //   return 0;
-    // }
-    // else print the names
     if (argc == 1) {
-      directory = opendir(".");
+      // struct dirent *ent;
+
+      // DIR *directory;
+
+      DIR *directory = opendir(".");
       if (directory == NULL){
         fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
+        return -1;
       }
       while ((ent = readdir(directory))){
         printf("%s\n", ent->d_name);
@@ -51,8 +30,12 @@ int main(int argc, char **argv)
     }
 
     for (int i = 0; i < argc; i++) {
+        // struct dirent *ent;
+
+        // DIR *directory;
+        DIR *directory = opendir(argv[i]);
+
         printf("%s\n", argv[i]);
-        directory = opendir(argv[i]);
         // ent = readdir(directory);
         if (directory == NULL){
           fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
