be399ee76a9b07784855efb58ff8959cedbb4d1d
Ta1grr <taile602@gmail.com>
2019-04-01 02:05:23 -0500

    it's 2am and it's finally done, I'm tired

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -5,50 +5,48 @@
 /**
  * Main
  */
-int main(int argc, char **argv)
+int listing(const char *path) 
 {
-  // Parse command line
-    struct dirent *ent;
+  // Declaring the ent pointer to struct dirent
+  struct dirent *ent;
 
-    // DIR *directory;
+  // Declaring buff struct
+  struct stat buf;
 
-    if (argc == 1) {
-      // struct dirent *ent;
+  // Calling stat using the full address and buf address
+  stat("./lsls.c", &buf);
 
-      // DIR *directory;
+  // Declaring the directory pointer with DIR and assigning it with opendir()
+  DIR *directory = opendir(path);
 
-      DIR *directory = opendir(".");
-      if (directory == NULL){
-        fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
-        return -1;
-      }
-      while ((ent = readdir(directory))){
-        printf("%s\n", ent->d_name);
-      }
-      closedir(directory);
-      return 0;
-    }
+  // Checking if the directory is NULL and returning proper from stderr
+  if (directory == NULL) 
+  {
+    // Using perror over fprintf(stderr, "strings") because it'll give a more precise error output.
+    perror("opendir");
+    return -1;
+  }
 
+  // While directory is not NULL it'll keep printing from readdir
+  while((ent = readdir(directory))){
+    printf("%10lld   %s\n", buf.st_size, ent->d_name);
+  }
+  closedir(directory);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+
+  // If argc is 1 then it'll run current directory
+  if (argc == 1){
+    listing(".");
+  }
+  else{
+    // else it'll iterate through argc and run the array of argv through lsls function and output accordingly.
     for (int i = 0; i < argc; i++) {
-        // struct dirent *ent;
-
-        // DIR *directory;
-        DIR *directory = opendir(argv[i]);
-
-        printf("%s\n", argv[i]);
-        // ent = readdir(directory);
-        if (directory == NULL){
-          fprintf(stderr, "ERROR MESSAGE: Directory is NULL\n");
-          return -1;
-        }
-        while((ent = readdir(directory))){
-          puts(ent->d_name);
-        }
-        closedir(directory);
-      }
-        // Close directory        
-        // closedir(directory);
-    
+      listing(argv[i]);
+    }
+  }
 
   return 0;
 }
